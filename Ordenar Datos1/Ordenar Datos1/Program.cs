f5fe33559cb0073b31299fb40fc6eb46917e3ef0
Erick Tellez <ebguido@uamv.edu.ni>
2024-10-10 22:31:38 -0600

    Funciones

---
+++ Ordenar Datos1/Ordenar Datos1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordenar_Datos1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
